4cf1e444f408997ba1a55c7d24576b631d33de90
Mahesh <mahesh.maddela@gmail.com>
2026-01-01 21:59:44 +0530

    Add simple ML model and dependencies

--- train.py
+++ train.py
@@ -1,1 +1,22 @@
-print("Training script running")
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+
+# Dummy dataset
+data = {
+    'square_feet': [1000, 1500, 2000, 2500],
+    'bedrooms': [2, 3, 3, 4],
+    'price': [200000, 250000, 300000, 350000]
+}
+
+df = pd.DataFrame(data)
+
+# Features and target
+X = df[['square_feet', 'bedrooms']]
+y = df['price']
+
+# Train a simple linear regression model
+model = LinearRegression()
+model.fit(X, y)
+
+print("Model trained successfully")
+print("Predicted price for 1800 sqft, 3 bedrooms:", model.predict([[1800, 3]])[0])
